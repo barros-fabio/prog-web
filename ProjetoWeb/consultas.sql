3dd00f9108005f3915ef38649b365517c41b8ec8
barros-fabio <fabiobarros@alunos.utfpr.edu.br>
2016-05-17 10:03:31 -0300

    Adicionando sql

---
+++ ProjetoWeb/consultas.sql
@@ -0,0 +1,9 @@
+INSERT INTO Professor(nomeProf,usuario,senha,email) VALUES("Fabio Barros", "fabiobarros","1234","fbarros640@gmail.com" );
+INSERT INTO Professor(nomeProf,usuario,senha,email) VALUES("João César", "jcesar","1234","jcesar@gmail.com" );
+INSERT INTO Aluno(nomeAluno, CPF, RA, curso, periodo, orientador) VALUES ("José Silva","12313","21313","Eng. Computação","2",1);
+SELECT * from Professor;
+SELECT * from Aluno;
+SELECT * from TCC;
+
+DELETE FROM Professor;
+DELETE FROM Aluno;
