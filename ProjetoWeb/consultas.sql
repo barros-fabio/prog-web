3ed59d3d17ed9b058eeb5e07fb0bab77ee6ae1b0
barros-fabio <fabiobarros@alunos.utfpr.edu.br>
2016-05-31 15:27:52 -0300

    introJS adicionado

--- ProjetoWeb/consultas.sql
+++ ProjetoWeb/consultas.sql
@@ -1,6 +1,7 @@
 INSERT INTO Professor(nomeProf,usuario,senha,email) VALUES("Fabio Barros", "fabiobarros","1234","fbarros640@gmail.com" );
 INSERT INTO Professor(nomeProf,usuario,senha,email) VALUES("João César", "jcesar","1234","jcesar@gmail.com" );
 INSERT INTO Aluno(nomeAluno, CPF, RA, curso, periodo, orientador) VALUES ("José Silva","12313","21313","Eng. Computação","2",1);
+INSERT INTO Aluno(nomeAluno, CPF, RA, curso, periodo, orientador) VALUES ("Pedro Souza","12313112","2133313","Engenharia de Computação","9",2);
 SELECT * from Professor;
 SELECT * from Aluno;
 SELECT * from TCC;
